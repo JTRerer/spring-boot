251b28f1923d299b4dc1333c4d4808a5f08dda6a
JTRerer <trongan147895@gmail.com>
2024-09-23 17:02:18 +0700

    First commit

---
+++ 01-spring-boot-overview/03-actuator-demo/src/test/java/com/yuu/springboot/demo/firstspringboot/FirstspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuu.springboot.demo.firstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
