524d5f55e96e7b20af621ea0b6a492460bf631ed
JTRerer <trongan147895@gmail.com>
2024-09-27 17:33:45 +0700

    Querying Objects with JPA + Updating Object with JPA + Deleting Object with JPA + Create Database Tables from Java Code

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/yuu/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/yuu/cruddemo/dao/StudentDAO.java
@@ -2,10 +2,22 @@ package com.yuu.cruddemo.dao;
 
 import com.yuu.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
+
+    void update(Student student);
+
+    void delete(Integer id);
+
+    int deleteAll();
 
 }
